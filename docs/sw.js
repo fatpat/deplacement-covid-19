d14f536d000711c36c0cf5d66136931d61f4b3c1
Jérôme LOYET <822436+fatpat@users.noreply.github.com>
2020-04-11 16:51:20 +0200

    my own version

---
+++ docs/sw.js
@@ -0,0 +1,117 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "android-chrome-192x192.4ad985c0.png",
+    "revision": "71c32dc8b4da4840d61a1796a2bc230b"
+  },
+  {
+    "url": "android-chrome-512x512.47463c43.png",
+    "revision": "8bf8012c4f4ae251d660b4739171b7ed"
+  },
+  {
+    "url": "apple-touch-icon-120x120-precomposed.5fbfa6af.png",
+    "revision": "a6e447e90b691da286286ab18849adac"
+  },
+  {
+    "url": "apple-touch-icon-120x120.ad0fe6f0.png",
+    "revision": "a6e447e90b691da286286ab18849adac"
+  },
+  {
+    "url": "apple-touch-icon-precomposed.887585e1.png",
+    "revision": "de5e22b15cf20cb0d586a036d714ee3f"
+  },
+  {
+    "url": "apple-touch-icon.aaba99a7.png",
+    "revision": "de5e22b15cf20cb0d586a036d714ee3f"
+  },
+  {
+    "url": "certificate.9e278a37.pdf",
+    "revision": "343ac8bb3542a6578f2a4cbaf0f99679"
+  },
+  {
+    "url": "certificate.bed43037.css",
+    "revision": "ccba00d64e98492458308266ad555c80"
+  },
+  {
+    "url": "confidentialite.c83648bb.css",
+    "revision": "0b9f3fd8536f7b602fc7fa69d6430fa9"
+  },
+  {
+    "url": "confidentialite.c83648bb.js",
+    "revision": "9858b680483e62c46cacaefcfd0711a2"
+  },
+  {
+    "url": "confidentialite.html",
+    "revision": "7dff74dd61c5af51be8a28dca0345cb0"
+  },
+  {
+    "url": "favicon-16x16.a0a61c08.png",
+    "revision": "652605ae0182d51781ab0be639ad2bda"
+  },
+  {
+    "url": "favicon-32x32.bd6b9d71.png",
+    "revision": "674c9c4ef1e4c7ea9de1218ee0bfd0cf"
+  },
+  {
+    "url": "index.html",
+    "revision": "ce00af0b27cbe317dd18405b4057f3f1"
+  },
+  {
+    "url": "logo_dnum.411ea756.svg",
+    "revision": "79cf40d4bdaa3a5de3475d90800005cf"
+  },
+  {
+    "url": "marianne-bold-webfont.db730b38.woff",
+    "revision": "0bcc99dd4adfb78e11098fedfe531cbb"
+  },
+  {
+    "url": "marianne-bold-webfont.f8428997.woff2",
+    "revision": "e67f6cefe32cc39f909e605c8d6337a9"
+  },
+  {
+    "url": "marianne-regular-webfont.0529dd78.woff2",
+    "revision": "d2c09e5f58d8360f541e2a8726c33587"
+  },
+  {
+    "url": "marianne-regular-webfont.78905f1b.woff",
+    "revision": "89f4f2326c77429e98693cf83703face"
+  },
+  {
+    "url": "MIN_Interieur_RVB.6e85e781.svg",
+    "revision": "75f5937c93153461f08e8723cf937a12"
+  },
+  {
+    "url": "safari-pinned-tab.a198a050.svg",
+    "revision": "f53452e6ac8760f12bab91672e91d39b"
+  },
+  {
+    "url": "./",
+    "revision": "f8749bb03a1a7ac175c2155275f5abd6"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/deplacement-covid-19/index.html"));
